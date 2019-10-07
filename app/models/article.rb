c8baa5e6a79329ab5bfde955daf4ff04908f7c24
Cai heng <ianyaheng822@gmail.com>
2019-10-07 12:55:36 +1100

    create a article action and template

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 
 
 class Article < ApplicationRecord
-  validates :title, presence: true, length:{minimun:3, maximun: 50}
-  validates :description, presence: true, length:{minimun:10, maximun: 300}
+  validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  validates :description, presence: true, length:{minimum:10, maximum: 300}
   
 end
