ba21df8083ce59a29ad4c279a77d17a9ddb92a4e
Cai heng <ianyaheng822@gmail.com>
2019-10-08 16:22:18 +1100

    add the relation between users and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
+  
 class User < ApplicationRecord
-  has_many :comments
-
-  validates :username, presence: true, uniqueness:{case_sensitive: fsalse}, length: {minimum: 3, maximum: 25},
+  before_save { self.email = email.downcase }
+  has_many :articles, dependent: :destroy
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, 
+            length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 105},uniqueness:{case_sensitive: fsalse},
-            format:  {with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, length: { maximum: 105 },
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
 end
