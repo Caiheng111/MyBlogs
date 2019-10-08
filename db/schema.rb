be24b646b4f1d3653a3ecd22ac60ffdf8875cc41
Cai heng <ianyaheng822@gmail.com>
2019-10-08 16:58:33 +1100

    secure password attribute to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_045116) do
+ActiveRecord::Schema.define(version: 2019_10_08_055211) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_10_08_045116) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
