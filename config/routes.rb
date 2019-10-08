1cd7549d43852e132f3c8d51f1c3ec2d19c25a30
Cai heng <ianyaheng822@gmail.com>
2019-10-08 22:08:24 +1100

    sign up users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :users
+  # resources :comments
+  # resources :users
   root "pages#home"
   get "about", to: "pages#about"
-
   resources :articles
+  get "signup", to: "users#new"
+
+  post "users", to: "users#create"
+ 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
