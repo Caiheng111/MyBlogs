994b08ed70cda4442ebd96578d806bcf36396db5
Cai heng <ianyaheng822@gmail.com>
2019-10-08 15:08:17 +1100

    add the user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_many :comments
+
+  validates :username, presence: true, uniqueness:{case_sensitive: fsalse}, length: {minimum: 3, maximum: 25},
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 105},uniqueness:{case_sensitive: fsalse},
+            format:  {with: VALID_EMAIL_REGEX }
 end
