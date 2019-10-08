3f69b4b3a2ecbab3ebf822396cb6173666c7edaf
Cai heng <ianyaheng822@gmail.com>
2019-10-08 13:53:18 +1100

    style article index page

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@
 
 class Article < ApplicationRecord
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
-  validates :description, presence: true, length:{minimum:10, maximum: 3000}
+  validates :description, presence: true, length:{minimum:10, maximum: 30000}
   
 end
