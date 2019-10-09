d93ffc43c7367174e5be3e235248bd413370561b
Cai heng <ianyaheng822@gmail.com>
2019-10-09 15:01:52 +1100

    add show action and gravatar image

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,5 @@ Rails.application.routes.draw do
   post "users", to: "users#create"
   resources :users, except: [:new]
  
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
