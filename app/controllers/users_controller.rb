261cd5a5e138ce95e18d93fff69076af494065f5
Cai heng <ianyaheng822@gmail.com>
2019-10-10 16:28:58 +1100

    add sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user=User.new(user_params)
     if @user.save
+
       session[:user_id] = @user.id
-      flash[:success]="Welcome to the heng Blog #{@user.username}"
+      flash[:success]="Welcome to the HY Blog #{@user.username}"
       redirect_to user_path(@user)
     else
       render 'new'
